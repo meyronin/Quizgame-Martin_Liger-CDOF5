0f4969cc867b0e36c3a4a991466b4fb803f2e46e
AIMalvin <malvin.noel@edu.devinci.fr>
2024-01-29 19:31:40 +0100

    Randomize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# main.py
 from quizgame.game import QuizGame
 from quizgame.questions import QuizQuestion
+from random import shuffle
 
 if __name__ == "__main__":
     game = QuizGame()
@@ -8,4 +8,5 @@ if __name__ == "__main__":
         QuizQuestion("What is 2 + 2?", ["a) 3", "b) 4", "c) 5"], "b"),
         QuizQuestion("What is the capital of France?", ["a) London", "b) Paris", "c) Berlin"], "b"),
     ]
+    shuffle(questions)
     game.start_game(questions)
